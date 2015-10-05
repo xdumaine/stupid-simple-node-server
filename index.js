4a093804cb23d1497a3b87fa1ee951b78f5f65f4
Xander Dumaine <xander.dumaine@inin.com>
2015-10-05 15:50:30 -0400

    remove unused https

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var https = require('https');
-var http = require('http');
 var pem = require('pem');
 
 pem.createCertificate({ days:1, selfSigned:true }, function(err, keys) {
